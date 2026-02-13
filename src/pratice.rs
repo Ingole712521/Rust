7a1abe61cc6beaa4fe8054c63dc7f393f9838811
Ingole712521 <nehal712521@gmail.com>
2026-02-13 12:50:00 +0530

    Add data type examples and conversion function to pratice module

--- src/pratice.rs
+++ src/pratice.rs
@@ -21,7 +21,6 @@ pub fn pratice() {
     //     println!("The value of x is {}", x)
     // }
 
-
     // {Print multiple variable }
 
     // fn multiple_variable(){
@@ -31,15 +30,80 @@ pub fn pratice() {
     //     println!("The value of a and b is {} and {} ", a ,b );
     // }
 
+    // Data Type Questions
 
-// Data Type Questions
+    // i32 and u32 variable
+
+    // fn multiple_variable(){
+    //     let a =20;
+    //     let b: u32 = 10;
+    //     println!("The value of a and b is {} and {}" , a ,b);
+    // }
+
+    // fn floating(){
+    //     let a = 3.5;
+    //     println!("The value of a floating is {}" , a );
+    // }
+
+    // fn boolean()-> bool{
+    //     let a = true;
+    //     let b = false;
+    // let number = 2 ;
+
+    //     if number > 10 {
+    //         println!("The value is greater than 10");
+    //          a
+    //     }
+    //     else{
+    //         println!("The value is less than 10");
+    //         b
+    //     }
+    // }
+
+    // Char
+
+    // fn character() {
+    //     let ch = "Text";
+    //     println!("The value of ch is {}", ch)
+    // }
+
+    // Tuples and access value
+
+    // fn tuples() {
+    //     let person = ("solana ", 52);
+    //     println!("{}", person.0);
+    //     println!("{}", person.1);
+    // }
+
+    // fn arr() {
+    //     let arr = [2, 3, 6, 7, 2];
+    //     println!("{:?}", arr[0])
+    // }
+
+
+    // fn lengtharr(){
+    //     let arr = [2,3,4,5,6,7,8,2,4];
+    //     print!("The length of the array is {}", arr.len());
+    // }
+
+
+    // COnvert into the float
+    fn convert(){
+        let a = 5;
+        let b = a as f64;
+        println!("THe b is {}", b)
+    }
 
 
 
 
-
-
-
+    convert()
+    // lengtharr();
+    // arr();
+    // tuples();
+    // character();
+    // boolean();
+    // floating();
     // multiple_variable();
     // shadow();
     // pi();
