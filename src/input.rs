99d80e834d9a888bd0964a655e680ca4811081c0
Ingole712521 <nehal712521@gmail.com>
2026-02-13 15:26:26 +0530

    Refactor input function to improve structure and add positive/negative check

--- src/input.rs
+++ src/input.rs
@@ -1,7 +1,6 @@
 use std::io;
 
-pub fn input(){
-
+pub fn input() {
     // name and print
     // fn print(){
     //     let mut name = String::new();
@@ -12,28 +11,70 @@ pub fn input(){
     //     println!("The value of num is {}" , num)
     // }
 
+    // sum
+    // fn sumdigit(){
 
-    // sum 
-// fn sumdigit(){
+    //     let mut a = String::new();
+    //     let mut b = String::new();
 
-//     let mut a = String::new();
-//     let mut b = String::new();
+    //     io::stdin().read_line(&mut a).unwrap();
+    //     io::stdin().read_line(&mut b).unwrap();
+
+    //     let a = a.trim().parse().unwrap();
+    //     let b = b.trim().parse().unwrap();
+
+    //     let sum = a + b;
+    //     println!("the sum value is {}", sum);
+    // }
+
+    // if else
+
+    // fn odd() {
+    //     let mut a = String::new();
+    //     io::stdin().read_line(&mut a).unwrap();
+    //     let a: i32 = a.trim().parse().unwrap();
+
+    //     if a % 2 == 0 {
+    //     } else {
+    //         println!("The number is oddNumber ");
+    //     }
+    // }
+
+    // Largest of two
+
+    // fn largest() {
+    //     let mut a = String::new();
+    //     let mut b = String::new();
+
+    //     io::stdin().read_line(&mut a).unwrap();
+    //     io::stdin().read_line(&mut b).unwrap();
+
+    //     if a > b {
+    //         println!("A is bigger than b");
+    //     } else {
+    //         println!("B is bigger than a ")
+    //     }
+    // }
+
+    // POsitive Negative
+
+    fn postive() {
+        let mut a = String::new();
+        io::stdin().read_line(&mut a).unwrap();
+        let a: i32 = a.trim().parse().unwrap();
+
+        if a > 0 {
+            println!("Positive");
+        }
+        else if a < 0{
+            println!("Negative");
+        }
+        else{
+            println!("Zero")
+        }
+    }
 
 
-//     io::stdin().read_line(&mut a).unwrap();
-//     io::stdin().read_line(&mut b).unwrap();
-    
-//     let a = a.trim().parse().unwrap();
-//     let b = b.trim().parse().unwrap();
-    
-//     let sum = a + b;
-//     println!("the sum value is {}", sum);
-// }
-
-
-
-
-    
 
 
 
@@ -41,6 +82,13 @@ pub fn input(){
 
 
 
-// sumdigit();
+
+
+
+
+    postive();
+    // largest();
+    // odd();
+    // sumdigit();
     // print();
 }
