5402626ff12edfa46da4b23c4efb81e4d3b603ce
Ingole712521 <nehal712521@gmail.com>
2026-02-16 12:28:06 +0530

    learned abd pratice of rust factorial question solve by myself

--- src/circle.rs
+++ src/circle.rs
@@ -31,22 +31,35 @@ pub fn circle() {
     //     }
     // }
 
-    fn return(){
-        let mut num = String ::new();
-        io::stdin().read_line(&mut num).unwrap();
-        let num : i32 = num.trim().parse().unwrap();
+    // fn chala() {
+    //     let mut num = String::new();
+    //     io::stdin().read_line(&mut num).unwrap();
 
-        for num in 1..num {
-            num *=num
+    //     let nums = num.trim().parse().unwrap();
+
+    //     for nums in 1..=nums {
+    //         if nums == 6 {
+    //             continue;
+    //         }
+    //         println!("{}", nums)
+    //     }
+    // }
+
+    fn factorial() {
+        let mut num = String::new();
+        io::stdin().read_line(&mut num).unwrap();
+        let number: i32 = num.trim().parse().unwrap();
+        let mut fact = 1;
+
+        for number in 1..=number {
+            fact *= number;
         }
+        println!("{}", fact)
     }
 
+    factorial();
 
-
-
-
-
-
+    // chala();
     // breakh();
     // value();
     // even();
