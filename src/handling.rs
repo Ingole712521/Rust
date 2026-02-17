d7d802a6bed0d9eb4ebcacd5e34561a968a9b0e6
Ingole712521 <nehal712521@gmail.com>
2026-02-17 15:16:12 +0530

    Refactor handling module; comment out unused practice functions and update check_age implementation

--- src/handling.rs
+++ src/handling.rs
@@ -1,17 +1,50 @@
 pub fn handling() {
+    // fn pratice() {
+    //     fn divide(a: i32, b: i32) -> Result<i32, String> {
+    //         if b == 0 {
+    //             Err(String::from("Cannot diived by zero"))
+    //         } else {
+    //             Ok(a / b)
+    //         }
+    //     }
+
+    //     match divide(100, 20) {
+    //         Ok(value) => println!("the result is {}", value),
+    //         Err(err) => println!("{}", err),
+    //     }
+    // }
+
+    // fn pratice() {
+    //     fn check_age(age: i32) -> Result<String, String> {
+    //         if age == 0 {
+    //             Err(String::from("Not eligible"))
+    //         } else if age >= 18 {
+    //             Ok(String::from("eligible"))
+    //         } else {
+    //             Err(String::from("Not eligible"))
+    //         }
+    //     }
+
+    //     match check_age(90) {
+    //         Ok(value) => println!("{}", value),
+    //         Err(err) => println!("{}", err),
+    //     }
+    // }
+
     fn pratice() {
-        fn divide(a: i32, b: i32) -> Result<i32, String> {
-            if b == 0 {
-                Err(String::from("Cannot diived by zero"))
+        fn check_age(age: i32) -> Result<String, String> {
+            if age > 18 {
+                Ok(String::from("Eligible"))
             } else {
-                Ok(a / b)
+                Err(String::from("Not Eligible"))
             }
         }
 
-        match divide(100, 20) {
-            Ok(value) => println!("the result is {}", value),
+        match check_age(20) {
+            Ok(value) => println!("{}", value),
             Err(err) => println!("{}", err),
         }
     }
+
     pratice();
 }
