d8ea7be7f69c456d9218c9b72766d1282210894a
Ingole712521 <nehal712521@gmail.com>
2026-03-05 18:04:23 +0530

    Added the buuble sort algorithm

--- src/bubble.rs
+++ src/bubble.rs
@@ -1,5 +1,31 @@
 pub fn bubble() {
+    fn bubblealg(arr: &mut [i32]) {
+        let n = arr.len();
 
-    
+        for i in 0..n {
+            for j in 0..n - 1 - i {
+                if arr[j] > arr[j + 1] {
+                    arr.swap(j, j + 1)
+                }
+            }
+        }
+    }
 
+    fn callbuble() {
+        let mut number = [1, 3, 6, 2, 4, 5];
+
+        bubblealg(&mut number);
+        println!("{:?}", number)
+    }
+
+    callbuble();
+}
+
+fn bubblealg(arr: &mut [i32]) {
+    let n = arr.len();
+    for i in 0..n {
+        for j in 0..n - 1 - i {
+            arr.swap(j, j + 1);
+        }
+    }
 }
